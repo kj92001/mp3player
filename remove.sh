f7a9a51315a075d29ee407d6ab333dfb57532e48
김종광 <kj92001@gamil.com>
2026-06-28 17:13:14 +0900

    3th

---
+++ remove.sh
@@ -0,0 +1,4 @@
+sudo rm -rf /usr/local/bin/mp3player
+sudo rm -rf /usr/share/appcations/mp3player.desktop
+sudo rm -rf ~/.local/share/my_player
+sudo rm -rf /usr/share/icons/hicolor/scalable/apps/mp3player.svg
